df8975cf42beef9fb4280238719fa4e02b58e54c
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-31 21:17:18 +0300

    creating project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-light": "#E0BBE4",
+        "purple-medium": "#957DAD",
+        "purple-dark": "#4B2D5A",
+        "purple-accent": "#D291BC",
+        "purple-deep": "#512D6D",
+      },
+    },
   },
   plugins: [],
 };
