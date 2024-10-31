cea53ab59ffe1d6038a6cdcfc461ff7ee8fe1e44
Ludis <leulsegedmelaku1020@gmail.com>
2024-10-31 23:21:01 +0300

    Refactor App component to include routing for Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components";
+
 const App = () => {
-  
   return (
     <div className="h-screen overflow-hidden flex flex-col items-center justify-start bg-gradient-to-br from-purple-start to-purple-end text-gray-900 font-sans p-6">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 };
