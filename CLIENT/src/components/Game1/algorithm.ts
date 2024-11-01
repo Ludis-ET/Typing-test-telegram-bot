9d20b9864007635015042bf1add4db818ba01b02
Ludis <leulsegedmelaku1020@gmail.com>
2024-11-02 00:22:13 +0300

    Remove unused calculations for missed and extra characters in WPM algorithm

--- CLIENT/src/components/Game1/algorithm.ts
+++ CLIENT/src/components/Game1/algorithm.ts
@@ -10,8 +10,8 @@ export const calculateWPMAndAccuracy = (
   const totalTimeInMinutes = promptText.length / 5 / (totalChars / 60);
   const wpm = Math.round(totalChars / 5 / totalTimeInMinutes);
 
-  const missedChars = Math.max(0, totalChars - promptText.length);
-  const extraChars = Math.max(0, totalChars - (correctChars + missedChars));
+//   const missedChars = Math.max(0, totalChars - promptText.length);
+//   const extraChars = Math.max(0, totalChars - (correctChars + missedChars));
   const accuracyPercentage = ((correctChars / totalChars) * 100).toFixed(2);
   const accuracy = `${correctChars}/${totalChars} - Accuracy: ${accuracyPercentage}%`;
 
