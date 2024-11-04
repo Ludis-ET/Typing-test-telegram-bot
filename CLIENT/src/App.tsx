0f6baa4155b322c7fd951f4d1d2e89cc397a3b1b
Ludis <leulsegedmelaku1020@gmail.com>
2024-11-04 20:08:26 +0300

    Add console log for address in App component for debugging purposes

--- CLIENT/src/App.tsx
+++ CLIENT/src/App.tsx
@@ -17,6 +17,7 @@ const App = () => {
     };
     onConnect();
   }, []);
+  console.log(address)
   return (
     <div className="relative h-screen overflow-hidden flex flex-col items-center justify-start font-sans text-purple-light p-6">
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:#000;]"></div>
