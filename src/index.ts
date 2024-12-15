05d50d336e8799e3c36bc963c83ee9794e15b622
Ludis <leulsegedmelaku1020@gmail.com>
2024-12-15 11:05:49 +0300

    Update .gitignore and enhance server error handling: add .vercel to ignored files and improve app.listen with error logging

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import "./bot/bot";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err);
+  });
